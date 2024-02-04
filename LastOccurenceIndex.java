33e2e9999e2e9729ecef379a31fba7ed3db628c2
skjainul1999 <54411936+skjainul1999@users.noreply.github.com>
2024-02-04 15:30:09 +0530

    Update LastOccurenceIndex.java

--- LastOccurenceIndex.java
+++ LastOccurenceIndex.java
@@ -1,1 +1,26 @@
 
+// Question Link -- https://course.acciojob.com/idle?question=19c1d7f8-13a4-4da2-83d3-4164bacb02c5
+
+
+import java.util.*;
+public class Main{
+    public static void main(String[] hi){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int t=sc.nextInt();
+        System.out.println(lastOccurenceIndex(a,n,t,n-1));
+    }
+    static int lastOccurenceIndex(int a[], int n, int t,int startIndex){
+        if(startIndex == -1){
+            return -1;
+        }
+        if(a[startIndex] == t){
+            return startIndex;
+        }
+        return lastOccurenceIndex(a,n,t,startIndex-1);
+    }
+}
